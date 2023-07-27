67797b4ae384b426762f51f8b753113d079a0555
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro.local>
2023-07-27 14:32:09 -0700

    base authentication system

--- users/models.py
+++ users/models.py
@@ -1,9 +1,1 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-
-
-# class Account(models.Model):
-#     user = models.models.OneToOneField(User, on_delete=models.CASCADE)
-#     level = models.CharField(
-#         choices=[('Level_1', '1'), ('Level_2', '2'), ('Level_3', '3'), ('Level_3', '3'),]
-#     )
+from django.db import models

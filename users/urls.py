59e18a680b50ca0d3a23990b3a7e0321e951e499
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro.local>
2023-07-27 11:57:49 -0700

    basic registration

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login_user, name="login"),
-    path('logout_user', views.logout_user, name='logout'),
+    path('logout_user/', views.logout_user, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+import time
 
 def login_user(request):
 	if request.method == "POST":
@@ -22,3 +24,26 @@ def logout_user(request):
 	logout(request)
 	messages.success(request, ("You Were Logged Out!"))
 	return redirect('login')
+
+def register(request):
+	if request.user.is_authenticated:
+		messages.success(request, ('You are already logged in'))
+		return redirect('dashboard')
+
+	else:
+		if request.method == "POST":
+			form = UserCreationForm(request.POST)
+			if form.is_valid():
+				form.save()
+				username = form.cleaned_data['username']
+				password = form.cleaned_data['password1']
+				user = authenticate(username=username, password=password)
+				login(request, user)
+				messages.success(request, ("Registration Successful"))
+				return redirect('dashboard')
+
+		else:
+			form = UserCreationForm()
+		return render(request, 'authenticate/register.html', {
+			'form':form,
+		})

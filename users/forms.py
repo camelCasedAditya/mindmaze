bb0e47e77620101bedfa57eec5ba5ef898c9567c
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro.local>
2023-07-27 16:49:18 -0700

    improved name validation

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.validators import RegexValidator
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'type':'text'}), validators=[RegexValidator('[1234567890]', inverse_match=True)])
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}), validators=[RegexValidator('[1234567890]', inverse_match=True)])
     
     class Meta:
         model = User

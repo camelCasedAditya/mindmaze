b63b2e269c1d2e26702b11d98fae77a02234d5bb
nz <nzhou13@gmail.com>
2023-09-30 08:01:07 -0700

    Add Term class and week info to puzzles

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Term(models.Model):
+    year = models.IntegerField()
+    season = models.CharField(max_length=10)
+    length = models.IntegerField(default=1)
+    current_week = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.year)+" "+self.season
--- puzzles/views.py
+++ puzzles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Puzzle
+from dashboard.models import Term
 from .forms import AnswerForm
 from users.models import Submission
 from django.contrib.auth.decorators import login_required
@@ -31,11 +32,7 @@ def index(request):
     return render(request, "puzzles/index.html", context)
 
 @login_required
-def detail(request, puzzle_id_detail):
-    puzzle = get_object_or_404(Puzzle, pk=puzzle_id_detail)
-    past_submission = Submission.objects.filter(user = request.user, puzzle_id = puzzle_id_detail)
-    answered = past_submission.exists()
-
+def detail(request, term_id, week, puzzle_num):
     level_set = Group.objects.filter(user = request.user)
     for g in level_set:
         groupname = g.name
@@ -51,23 +48,25 @@ def detail(request, puzzle_id_detail):
         user_level = 5
     elif groupname == 'Default':
         user_level = 1
-    problem_set = Puzzle.objects.filter(level = user_level)
-    completed_set = Submission.objects.filter(user = request.user).values_list('puzzle_id', flat=True)
-    for i in completed_set:
-        problem_set = problem_set.exclude(pk = i)
-
     
-    problem_set = list(problem_set)
-    puzzle_index = problem_set.index(puzzle)
-    problem_set_len = len(problem_set)
+    weekly_set = Puzzle.objects.filter(term = Term.objects.all()[term_id-1], week = week, level = user_level)
+
+    puzzle = weekly_set[puzzle_num-1]
+    past_submission = Submission.objects.filter(user = request.user, puzzle = puzzle)
+    answered = past_submission.exists()
+    number_tries = len(past_submission)
+
+
+    puzzle_index = puzzle_num-1
+    problem_set_len = len(weekly_set)
     if (puzzle_index+1) > (problem_set_len-1):
-        next_puzzle = problem_set[0]
+        next_puzzle = weekly_set[0]
     else:
-        next_puzzle = problem_set[puzzle_index+1]
+        next_puzzle = weekly_set[puzzle_index+1]
     if (puzzle_index-1) < 0:
-        prev_puzzle = problem_set[problem_set_len-1]
+        prev_puzzle = weekly_set[problem_set_len-1]
     else:
-        prev_puzzle = problem_set[puzzle_index-1]
+        prev_puzzle = weekly_set[puzzle_index-1]
 
 
     if not answered:
@@ -77,10 +76,14 @@ def detail(request, puzzle_id_detail):
                 answered = True
                 student_answer = form.cleaned_data["student_answer"]
                 submission = Submission.objects.create(user=request.user, user_answer=student_answer, 
-                                                        puzzle_id=puzzle_id_detail, is_correct=(puzzle.solution==student_answer))
+                                                        puzzle=puzzle, is_correct=(puzzle.solution==student_answer))
                 return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": submission, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle})
         else:
             form = AnswerForm()
             return render(request, "puzzles/detail.html", {"puzzle": puzzle, "form": form, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle})
     else:
         return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": past_submission[0], "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle})
+    
+#@login_required
+#def weekly_homework(request):
+    #return render(request, "puzzles/week.html")

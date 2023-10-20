68b57064513c975a6d1dba96dfa81317620ad5e3
Aditya Chandrashekaran <aditya@Adityas-MacBook-Pro.local>
2023-10-19 19:38:47 -0700

    3 attempts bug fix

--- puzzles/views.py
+++ puzzles/views.py
@@ -6,9 +6,10 @@ from users.models import Submission
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
+
 @login_required
 def index(request):
-    level_set = Group.objects.filter(user = request.user)
+    level_set = Group.objects.filter(user=request.user)
     for g in level_set:
         groupname = g.name
     if groupname == 'Level_1':
@@ -24,29 +25,36 @@ def index(request):
     elif groupname == 'Default':
         user_level = 1
 
-    problem_set = Puzzle.objects.filter(level = user_level)
-    completed_set = Submission.objects.filter(Q(attempts=3) | Q(is_correct=True), user = request.user).values_list('puzzle_id', flat=True)
+    problem_set = Puzzle.objects.filter(level=user_level)
+    completed_set = Submission.objects.filter(Q(attempts=3) | Q(
+        is_correct=True), user=request.user).values_list('puzzle_id', flat=True)
     for i in completed_set:
-        problem_set = problem_set.exclude(pk = i)
+        problem_set = problem_set.exclude(pk=i)
     no_puzzles = False
     puzzles_left = list(problem_set)
     if (len(puzzles_left) == 0):
         no_puzzles = True
-    context = {"problem_set": problem_set, "no_puzzles":no_puzzles}
+    context = {"problem_set": problem_set, "no_puzzles": no_puzzles}
     return render(request, "puzzles/index.html", context)
+
 
 @login_required
 def detail(request, puzzle_id_detail):
     answered = False
     puzzle = get_object_or_404(Puzzle, pk=puzzle_id_detail)
-    past_submission = Submission.objects.filter(user = request.user, puzzle_id = puzzle_id_detail)
+    past_submission = Submission.objects.filter(
+        user=request.user, puzzle_id=puzzle_id_detail)
+
+    # Trys to get past attempts but if the submission object doesn't exist then it will set past attempts to 0
     try:
-        past_attempts = Submission.objects.filter(puzzle_id = puzzle_id_detail).values_list("attempts", flat=True)
+        past_attempts = Submission.objects.filter(
+            puzzle_id=puzzle_id_detail).values_list("attempts", flat=True)
         past_attempts = list(past_attempts)
         past_attempts = int(past_attempts[0])
     except:
         past_attempts = 0
 
+    # Finds the ammount of attempts left
     attempts_left = 3 - int(past_attempts)
     if (past_attempts == 0):
         attempts_left = 3
@@ -54,7 +62,7 @@ def detail(request, puzzle_id_detail):
     if past_attempts == 3:
         answered = True
 
-    level_set = Group.objects.filter(user = request.user)
+    level_set = Group.objects.filter(user=request.user)
     for g in level_set:
         groupname = g.name
     if groupname == 'Level_1':
@@ -69,10 +77,11 @@ def detail(request, puzzle_id_detail):
         user_level = 5
     elif groupname == 'Default':
         user_level = 1
-    problem_set = Puzzle.objects.filter(level = user_level)
-    completed_set = Submission.objects.filter(Q(attempts=3) | Q(is_correct=True), user = request.user).values_list('puzzle_id', flat=True)
+    problem_set = Puzzle.objects.filter(level=user_level)
+    completed_set = Submission.objects.filter(Q(attempts=3) | Q(
+        is_correct=True), user=request.user).values_list('puzzle_id', flat=True)
     for i in completed_set:
-        problem_set = problem_set.exclude(pk = i)
+        problem_set = problem_set.exclude(pk=i)
 
     next_puzzle_exists = True
     problem_set = list(problem_set)
@@ -87,50 +96,56 @@ def detail(request, puzzle_id_detail):
     else:
         prev_puzzle = problem_set[puzzle_index-1]
 
-
     if (past_attempts < 3):
         if request.method == "POST":
             form = AnswerForm(request.POST or None)
             if form.is_valid():
-                
+
                 if past_attempts > 0:
                     past_attempts += 1
                     student_answer = form.cleaned_data["student_answer"]
-                    submission = Submission.objects.filter(user=request.user, puzzle_id=puzzle_id_detail).update(user_answer=student_answer, is_correct=(puzzle.solution==student_answer), attempts=past_attempts)
-                    if (student_answer == puzzle.solution):
-                        past_attempts = 3
-                    if past_attempts == 3:
+                    submission = Submission.objects.filter(user=request.user, puzzle_id=puzzle_id_detail).update(
+                        user_answer=student_answer, is_correct=(puzzle.solution == student_answer), attempts=past_attempts)
+                    if past_attempts == 3 or Submission.objects.filter(user=request.user, puzzle_id=puzzle_id_detail, is_correct=True).exists():
                         answered = True
                     else:
                         answered = False
 
+                    problem_set = Puzzle.objects.filter(level=user_level)
+                    completed_set = Submission.objects.filter(Q(attempts=3) | Q(
+                        is_correct=True), user=request.user).values_list('puzzle_id', flat=True)
+                    for i in completed_set:
+                        problem_set = problem_set.exclude(pk=i)
                     if (len(problem_set) == 0):
                         next_puzzle_exists = False
                     form = AnswerForm(request.POST)
                     attempts_left = 3 - int(past_attempts)
-                    return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": submission, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled":next_puzzle_exists, "form":form, "student_answer":student_answer, "attempts_left":attempts_left})
+                    return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": submission, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled": next_puzzle_exists, "form": form, "student_answer": student_answer, "attempts_left": attempts_left})
 
                 else:
                     past_attempts += 1
                     student_answer = form.cleaned_data["student_answer"]
-                    if (student_answer == puzzle.solution):
-                        past_attempts = 3
-                    submission = Submission.objects.create(user=request.user, user_answer=student_answer, 
-                                                            puzzle_id=puzzle_id_detail, is_correct=(puzzle.solution==student_answer), attempts=past_attempts)
-                    if past_attempts == 3:
+                    submission = Submission.objects.create(user=request.user, user_answer=student_answer,
+                                                           puzzle_id=puzzle_id_detail, is_correct=(puzzle.solution == student_answer), attempts=past_attempts)
+                    if past_attempts == 3 or Submission.objects.filter(user=request.user, puzzle_id=puzzle_id_detail, is_correct=True).exists():
                         answered = True
                     else:
                         answered = False
+
+                    problem_set = Puzzle.objects.filter(level=user_level)
+                    completed_set = Submission.objects.filter(Q(attempts=3) | Q(
+                        is_correct=True), user=request.user).values_list('puzzle_id', flat=True)
+                    for i in completed_set:
+                        problem_set = problem_set.exclude(pk=i)
 
                     if (len(problem_set) == 0):
                         next_puzzle_exists = False
                     attempts_left = 3 - int(past_attempts)
                     if (past_attempts == 0):
                         attempts_left = 3
-                    return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": submission, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled":next_puzzle_exists, "form":form, "student_answer":student_answer, "attempts_left":attempts_left})
+                    return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": submission, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled": next_puzzle_exists, "form": form, "student_answer": student_answer, "attempts_left": attempts_left})
         else:
             form = AnswerForm()
-            return render(request, "puzzles/detail.html", {"puzzle": puzzle, "form": form, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled":next_puzzle_exists, "attempts_left":attempts_left})
+            return render(request, "puzzles/detail.html", {"puzzle": puzzle, "form": form, "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled": next_puzzle_exists, "attempts_left": attempts_left})
     else:
-        return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": past_submission[0], "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled":next_puzzle_exists, "attempts_left":attempts_left})
-    
+        return render(request, "puzzles/detail.html", {"puzzle": puzzle, "submission": past_submission[0], "answered": answered, "next_puzzle": next_puzzle, "prev_puzzle": prev_puzzle, "button_enabled": next_puzzle_exists, "attempts_left": attempts_left})
